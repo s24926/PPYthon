3188343ca6a3e8a2b2a675058135815ec3fbadea
s24926 <s24926@pjwstk.edu.pl>
2023-03-17 16:34:34 +0100

    zestaw 1

--- archskrypt.py
+++ archskrypt.py
@@ -1,3 +1,17 @@
 import webbrowser
+import Play_mp3
+import requests as requests
+
+filename = "robot_room.mp3"
+Play_mp3.play(filename)
+
 url = input("podaj adres strony")
 webbrowser.open(url)
+
+dates = ["20191111", "20191212", "20220101"]
+for i in range (3):
+    urls = "http://archive.org/wayback/available?url=" + url + "&timestamp=" + str(dates[i])
+    response = requests.get(urls)
+    d = response.json()
+    page = d["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
